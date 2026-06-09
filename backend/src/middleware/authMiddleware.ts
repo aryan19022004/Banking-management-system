adba7defce97a85bf3419d69e7c2f8197643b696
Aryan tiwari <at7123029@gmail.com>
2026-06-09 23:08:50 +0530

    Added a Authentication middleware

--- backend/src/middleware/authMiddleware.ts
+++ backend/src/middleware/authMiddleware.ts
@@ -1,28 +1,45 @@
-//In this we will implement the authentication middleware
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import User from "../models/user.js";
 
 const jwt_secret = process.env.JWT_SECRET;
 
-const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.cookies.token;
-    if (!token) {
-        return res.status(401).json({ message: "Unauthorized" });
-    }
+const authMiddleware = (allowedRoles: string[] = []) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const token = req.cookies.token;
 
-    try {
-        const decoded = jwt.verify(token, jwt_secret as string) as { _id: string };
-
-        const user = await User.findById(decoded._id);
-        if (!user) {
-            return res.status(401).json({ message: "Unauthorized " });
+        if (!token) {
+            return res.status(401).json({ message: "Unauthorized" });
         }
-        req.user = user;
-        next();
-    } catch (error) {
-        return res.status(401).json({ message: "Unauthorized" });
-    }
-}
+
+        try {
+            const decoded = jwt.verify(token, jwt_secret as string) as { _id: string };
+
+            const user = await User.findById(decoded._id).populate("userType", "name");
+
+            if (!user) {
+                return res.status(401).json({ message: "Unauthorized" });
+            }
+
+            req.user = user;
+
+
+            if (allowedRoles.length > 0) {
+                const role = (user.userType as any)?.name;
+
+                if (!allowedRoles.includes(role)) {
+                    return res.status(403).json({
+                        message: "Access Denied"
+                    });
+                }
+            }
+
+            next();
+
+        } catch (error) {
+            return res.status(401).json({ message: "Unauthorized" });
+        }
+    };
+};
 
 export default authMiddleware;
